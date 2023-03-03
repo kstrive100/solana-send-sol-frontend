842356af8c31a6220818399e97021d7c2b631ee6
kstrive <kstrive100@gmail.com>
2023-03-03 21:10:37 +0300

    first commit

---
+++ components/SendSolForm.tsx
@@ -0,0 +1,68 @@
+import { useConnection, useWallet } from '@solana/wallet-adapter-react';
+import { FC, useState } from 'react'
+import styles from '../styles/Home.module.css'
+import * as Web3 from '@solana/web3.js'
+import { LAMPORTS_PER_SOL } from '@solana/web3.js';
+
+
+// 5gkbhZdjUpdSYTsYGExa9WNkv1sV4CuJ3V4F6fwLKYk2
+
+export const SendSolForm: FC = () => {
+    
+    const [txSig, setTxSig] = useState('');
+	const { connection } = useConnection();
+	const { publicKey, sendTransaction } = useWallet();
+    const link = () => {
+        return txSig ? `https://explorer.solana.com/tx/${txSig}?cluster=devnet` : ''
+    }
+    
+    const sendSol = event => {
+        event.preventDefault()
+        if (!connection || !publicKey) { 
+			alert("Please connect your wallet first lol")
+			return
+		}
+        const transaction = new Web3.Transaction()
+        const recipientPubKey = new Web3.PublicKey(event.target.recipient.value)
+        
+        const sendSolInstruction = Web3.SystemProgram.transfer(
+            {
+                fromPubkey: publicKey,
+                toPubkey: recipientPubKey, 
+                lamports: LAMPORTS_PER_SOL * event.target.amount.value,
+            }
+        )
+    
+        transaction.add(sendSolInstruction)
+        sendTransaction(transaction, connection).then(sig => {
+            setTxSig(sig)
+		})
+
+        console.log(`Send ${event.target.amount.value} SOL to ${event.target.recipient.value}`)
+    }
+
+    return (
+        <div>
+            {
+                publicKey ?
+                    <form onSubmit={sendSol} className={styles.form}>
+                        <label htmlFor="amount">Amount (in SOL) to send:</label>
+                        <input id="amount" type="text" className={styles.formField} placeholder="e.g. 0.1" required />
+                        <br />
+                        <label htmlFor="recipient">Send SOL to:</label>
+                        <input id="recipient" type="text" className={styles.formField} placeholder="e.g. 4Zw1fXuYuJhWhu9KLEYMhiPEiqcpKd6akw3WRZCv84HA" required />
+                        <button type="submit" className={styles.formButton}>Send</button>
+                    </form> :
+                    <span>Connect Your Wallet</span>
+            }
+            {
+                txSig ?
+                    <div>
+                        <p>View your transaction on </p>
+                        <a href={link()}>Solana Explorer</a>
+                    </div> :
+                    null
+            }
+        </div>
+    )
+}
---
+++ components/WalletContextProvider.tsx
@@ -0,0 +1,24 @@
+import { FC, ReactNode } from "react";
+import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react'
+import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
+import * as web3 from '@solana/web3.js'
+import { GlowWalletAdapter, PhantomWalletAdapter } from '@solana/wallet-adapter-wallets';
+require('@solana/wallet-adapter-react-ui/styles.css')
+
+const WalletContextProvider: FC<{ children: ReactNode }> = ({ children }) => {
+    const wallets = [new PhantomWalletAdapter(), new GlowWalletAdapter()]
+
+    const endpoint = web3.clusterApiUrl('devnet')
+
+    return (
+        <ConnectionProvider endpoint={endpoint}>
+            <WalletProvider wallets={wallets}>
+                <WalletModalProvider>
+                    { children }
+                </WalletModalProvider>
+            </WalletProvider>
+        </ConnectionProvider>
+    )
+}
+
+export default WalletContextProvider
